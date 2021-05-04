45498044cf8acea225459d2969e945bd4ec7131f
LCottin <cottin_leonard@icloud.com>
2021-05-04 19:18:27 +0200

    Added second file to have a second channel of communication with the robot

---
+++ Emission2/Emission2.ino
@@ -0,0 +1,49 @@
+/**
+ * Code permettant d'envoyer les données 
+ * de l'acceléromètre au robot
+ */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//ports de sortie
+const byte x_out = A0;
+const byte y_out = A1;
+const byte z_out = A2;
+
+RF24 radio(7,8);
+const byte address[6] = "00001";
+
+struct dataToSend
+{
+    byte xAxis;
+    byte yAxis;
+    byte zAxis;
+} send_data;
+
+
+// ---------------------------------------- //
+// -                SETUP                 - //
+// ---------------------------------------- //
+void setup() 
+{
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setDataRate(RF24_2MBPS);
+    radio.stopListening();
+}
+
+
+// ---------------------------------------- //
+// -                LOOP                  - //
+// ---------------------------------------- //
+void loop() 
+{
+    send_data.xAxis = -1*analogRead(x_out);
+    send_data.yAxis = -1*analogRead(y_out);
+    send_data.zAxis = -1*analogRead(z_out);
+    radio.write(&send_data, sizeof(dataToSend));
+    delay(70);
+}

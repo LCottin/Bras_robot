f5c077ab897459db0ffc49aaedec9ae0d16d725b
LCottin <cottin_leonard@icloud.com>
2021-06-09 14:20:12 +0200

    Added third file to send data

---
+++ Emission3/Emission3.ino
@@ -0,0 +1,55 @@
+/**
+ * Code permettant d'envoyer les données 
+ * de l'acceléromètre au robot
+ */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//ports de sortie
+const byte x_out = A0;
+const byte y_out = A1;
+const byte z_out = A2;
+
+RF24 radio(9,10); //emission avec Arduino Nano-rf
+const byte address[6] = "00001";
+
+//structure de données pour l'envoie des inclinaisons 
+struct dataToSend
+{
+    short id;
+    short xAxis;
+    short yAxis;
+    short zAxis;
+} send_data;
+
+// ---------------------------------------- //
+// -                SETUP                 - //
+// ---------------------------------------- //
+void setup() 
+{
+    //Configuration de l'émetteur
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setDataRate(RF24_2MBPS);
+    radio.stopListening();
+
+    send_data.id = 3;
+}
+
+// ---------------------------------------- //
+// -                LOOP                  - //
+// ---------------------------------------- //
+void loop() 
+{
+    //lecture des données sur les 3 axes et inversion de signe
+    send_data.xAxis = analogRead(x_out);
+    send_data.yAxis = analogRead(y_out);
+    send_data.zAxis = analogRead(z_out);
+
+    //envoie des données lues
+    radio.write(&send_data, sizeof(dataToSend));
+    delay(10);
+}

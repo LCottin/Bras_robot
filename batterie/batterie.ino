b71053737a7471dbd0184329f56a62ce90c51989
LCottin <cottin_leonard@icloud.com>
2021-06-09 14:50:09 +0200

    Added arrays and steps

--- batterie/batterie.ino
+++ batterie/batterie.ino
@@ -3,13 +3,19 @@
  * la charge de la batterie
  */
 
+//Associe une led à un pin
 enum led {ROUGE = 3, 
           JAUNE1 = 5, JAUNE2 = 6, 
           VERT1 = 9, VERT2 = 10, VERT3 = 11};
 
-const byte pinLed[6] = {3, 5, 6, 9, 10, 11};
+//Stocke les pins des leds
+const byte pinLed[6] = {3, 5, 6, 9, 10, 11}
 
+//Pin de lecture de la tension de la batterie
 const byte tension = A0;
+
+//Différents paliers pour allumer ou non les led
+const short paliers[7] = {827, 850, 873, 896, 920, 943, 966}
 
 
 // ---------------------------------------- //
@@ -32,107 +38,106 @@ void setup()
 // ---------------------------------------- //
 void loop() 
 {
-   short lecture = analogRead(tension);
+  short lecture = analogRead(tension);
 
-}
+  Serial.println(lecture);
 
+  if(lecture < palier[0])
+  {
+    //Toutes les led sont éteintes
+    for (int i = 0; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], LOW);
+    }
+  }
 
-int batt;
+  else if((lecture >= palier[0] )&& (lecture < palier[1]))
+  {
+    //La premiere led s'allume progressivement
+    analogWrite(ROUGE, map(lecture, 827, 850, 0, 255));
 
-void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT); 
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT); 
-  pinMode(11, OUTPUT);
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
-}
+    for (int i = 1; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], LOW);
+    }
+  }
 
-void loop() {
-  batt = analogRead(A0);
-  Serial.println(batt);
-  if(batt < 827)
+  else if((lecture >= palier[1] )&& (lecture < palier[2]))
   {
-    analogWrite(3, LOW);
-    analogWrite(5, LOW);
-    analogWrite(6, LOW);
-    analogWrite(9, LOW);
-    analogWrite(10, LOW);
-    analogWrite(11, LOW);
-    digitalWrite(13, LOW);
-  }
-  if((batt >= 827 )&& (batt < 850))
-  {
-    analogWrite(3, map(batt, 827, 850, 0, 255));
-    analogWrite(5, LOW);
-    analogWrite(6, LOW);
-    analogWrite(9, LOW);
-    analogWrite(10, LOW);
-    analogWrite(11, LOW);
-    digitalWrite(13, LOW);
-  }
-  if((batt >= 850 )&& (batt < 873))
-  {
-    analogWrite(3, 255);
-    analogWrite(5, map(batt, 850, 873, 0, 255));
-    analogWrite(6, LOW);
-    analogWrite(9, LOW);
-    analogWrite(10, LOW);
-    analogWrite(11, LOW);
-    digitalWrite(13, LOW);
-  }
-  if((batt >= 873 )&& (batt < 896))
-  {
-    analogWrite(3, 255);
-    analogWrite(5, 255);
-    analogWrite(6, map(batt, 873, 896, 0, 255));
-    analogWrite(9, LOW);
-    analogWrite(10, LOW);
-    analogWrite(11, LOW);
-    digitalWrite(13, LOW);
-  }
-  if((batt >= 896 )&& (batt < 920))
-  {
-    analogWrite(3, 255);
-    analogWrite(5, 255);
-    analogWrite(6, 255);
-    analogWrite(9, map(batt, 896, 920, 0, 255));
-    analogWrite(10, LOW);
-    analogWrite(11, LOW);
-    digitalWrite(13, LOW);
-  }
-  if((batt >= 920 )&& (batt < 943))
-  {
-    analogWrite(3, 255);
-    analogWrite(5, 255);
-    analogWrite(6, 255);
-    analogWrite(9, 255);
-    analogWrite(10, map(batt, 920, 943, 0, 255));
-    analogWrite(11, LOW);
-    digitalWrite(13, LOW);
-  }
-  if((batt >= 943 )&& (batt < 966))
-  {
-    analogWrite(3, 255);
-    analogWrite(5, 255);
-    analogWrite(6, 255);
-    analogWrite(9, 255);
-    analogWrite(10, 255);
-    analogWrite(11, map(batt, 943, 966, 0, 255));
-    digitalWrite(13, LOW);
-  }
-  if(batt >= 966 )
-  {
-    analogWrite(3, 255);
-    analogWrite(5, 255);
-    analogWrite(6, 255);
-    analogWrite(9, 255);
-    analogWrite(10, 255);
-    analogWrite(11, 255);
-    digitalWrite(13, HIGH);
-  }
-  
+    //La premiere led est allumé et la seconde s'allume progressivement
+    digitalWrite(ROUGE, HIGH);
 
+    analogWrite(JAUNE1, map(lecture, 850, 873, 0, 255));
+
+    for (int i = 2; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], LOW);
+    }
+  }
+
+  else if((lecture >= palier[2] )&& (lecture < palier[3]))
+  {
+    //Les 2 premières led sont allumées et la 3e s'allume progressivement
+    digitalWrite(ROUGE, HIGH);
+    digitalWrite(JAUNE1, HIGH);
+    
+    analogWrite(JAUNE2, map(lecture, 873, 896, 0, 255));
+
+    for (int i = 3; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], LOW);
+    }
+  }
+
+  else if((lecture >= palier[3] )&& (lecture < palier[4]))
+  {
+    //Les 3 premières led sont allumées et la 4e s'allume progressivement
+    digitalWrite(ROUGE, HIGH);
+    digitalWrite(JAUNE1, HIGH);
+    digitalWrite(JAUNE2, HIGH);
+
+    analogWrite(9, map(lecture, 896, 920, 0, 255));
+
+    for (int i = 4; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], LOW);
+    }
+  }
+
+  else if((lecture >= palier[4] )&& (lecture < palier[5]))
+  {
+    //Les 4 premières led sont allumées et la 5e s'allume progressivement
+    digitalWrite(ROUGE, HIGH);
+    digitalWrite(JAUNE1, HIGH);
+    digitalWrite(JAUNE2, HIGH);
+    digitalWrite(VERT1, HIGH);
+
+    analogWrite(VERT2, map(lecture, 920, 943, 0, 255));
+
+    for (int i = 5; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], LOW);
+    }
+  }
+
+  else if((lecture >= palier[5] )&& (lecture < palier[6]))
+  {
+    //Les 5 premières led sont allumées et la 6e s'allume progressivement
+    digitalWrite(ROUGE, HIGH);
+    digitalWrite(JAUNE1, HIGH);
+    digitalWrite(JAUNE2, HIGH);
+    digitalWrite(VERT1, HIGH);
+    digitalWrite(VERT2, HIGH);
+
+    analogWrite(VERT3, map(lecture, 943, 966, 0, 255));
+  }
+
+  else if(lecture >= palier[6])
+  {
+    //Toutes les led sont allumées
+    for (int i = 0; i < 6; i++)
+    {
+      digitalWrite(pinLed[i], HIGH);
+    }
+  }
 }

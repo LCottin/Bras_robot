b40c7ebf0089a8644aea01eeff78cdd8aec311a2
LCottin <cottin_leonard@icloud.com>
2021-06-18 15:31:11 +0200

    Changed method of priting data on the screen, added method to clear, added variables for data like pins, actions ...

--- Telecommande/Telecommande.ino
+++ Telecommande/Telecommande.ino
@@ -19,28 +19,19 @@ RF24Network network(radio);
 const byte noeudMere  = 00; // Valeur "0" écrit au format "octal" (d'où l'autre "0" devant)
 const byte monNoeud   = 04;
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 //Pins des boutons
-//enum BOUTON {PIN_HAUT = 2, PIN_BAS = 3, PIN_PLAY = 4, PIN_PAUSE = 5, PIN_STOP = 6};
-#define PIN_HAUT  2
-#define PIN_BAS   3
-#define PIN_PLAY  4
-#define PIN_PAUSE 5
-#define PIN_STOP  6
+enum BOUTON {PIN_HAUT = 2, PIN_BAS = 3, PIN_PLAY = 4, PIN_PAUSE = 5, PIN_STOP = 6};
 
 //Interéaction avec l'écran
-#define NOMBRE_ORDRES 4
-byte numOrdre      = -1; 
+const byte NOMBRE_ORDRES = 4;
+byte numOrdre; 
 
-#define RIEN 10
-#define PLAY 11
-#define PAUSE 12
-#define STOP 13
-//enum ACTION {RIEN = 10, PLAY = 11, PAUSE = 12, STOP = 13};
-const char* ordreTexte[NOMBRE_ORDRES] = {"Colere", "Joie", "Surprise", "T_Reel"};
-const char  ordre[NOMBRE_ORDRES]      = {0, 1, 2, 3};
+enum ACTION {RIEN = 10, PLAY = 11, PAUSE = 12, STOP = 13};
 
+const char* ordreTexte[]     = {"Colere", "Joie", "Surprise", "T_Reel"};
+const char  ordre[NOMBRE_ORDRES]   = {0, 1, 2, 3};
 
 //structure de données pour l'envoie des ordres
 struct dataToSend
@@ -75,111 +66,113 @@ void setup()
     
     //init ecran
     u8g2.begin();
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB14_tr) ;  // sélection d'une police de caractère
+    u8g2.enableUTF8Print();
+    
+    u8g2.firstPage();
+    do {
+      u8g2.setFont(u8g2_font_ncenB14_tr);
+      u8g2.drawStr(10, 20, "Init ...");
+      delay(500);
+    } while (u8g2.nextPage());
 }
 
 void loop() 
 {
-  //vide le buffer
-  u8g2.clearBuffer();
-  network.update();
+    //vide le buffer
+    network.update();
   
-  if (digitalRead(PIN_HAUT) == HIGH)
-  {
+    if (digitalRead(PIN_HAUT) == HIGH)
+    {
+        
         numOrdre = (numOrdre + 1) % NOMBRE_ORDRES;
-        u8g2.drawStr(10, 15, ordreTexte[numOrdre]);
-  }
-  //    send_data.mode = HAUT;
-  //    radio.write(&mode, sizeof(char));
-  //    radio.startListening();
-  //    startTimer = millis();
-  //    timeout = false;
-  //    while ( !radio.available() && !timeout ) 
-  //    {
-  //      if (millis() - startTimer > 1000 ) 
-  //        timeout = true; //timed out
-  //    }
-  //    if (timeout)
-  //    {
-  //      u8g2.clearBuffer();
-  //      u8g2.drawStr(0, 10, "pb");
-  //      u8g2.drawStr(0, 28, "transmission");
-  //      u8g2.drawStr(0, 46, "!!!");
-  //    }
-  //    else
-  //    {
-  //      radio.read(&codeRetour, sizeof(char));
-  //      switch(codeRetour)
-  //      {
-  //        case 1 :
-  //          u8g2.clearBuffer();
-  //          u8g2.drawStr(40, 28, "Mode");
-  //          u8g2.drawStr(20, 48, "Emotion 1");
-  //          break;
-  //        case 2 :
-  //          u8g2.clearBuffer();
-  //          u8g2.drawStr(40, 28, "Mode");
-  //          u8g2.drawStr(20, 48, "Emotion 2");
-  //          break;
-  //        case 3 :
-  //          u8g2.clearBuffer();
-  //          u8g2.drawStr(40, 28, "Mode");
-  //          u8g2.drawStr(20, 48, "Emotion 3");
-  //          break;
-  //        case 4 :
-  //          u8g2.clearBuffer();
-  //          u8g2.drawStr(40, 28, "Cmd");
-  //          u8g2.drawStr(20, 48, "Tps reel");
-  //          break;
-  //        default :
-  //          u8g2.clearBuffer();
-  //          u8g2.drawStr(10, 28, "Pb Mode");
-  //          itoa(codeRetour, str, 10);
-  //          u8g2.drawStr(20, 48, str);
-  //          break;
-  //      }
-  //    }
-  //    radio.stopListening();
-  //  }
+        u8g2.firstPage();
+        do 
+        {
+          u8g2.setFont(u8g2_font_ncenB14_tr);
+          u8g2.drawStr(10, 50, ordreTexte[numOrdre]);
+        } while (u8g2.nextPage());
+        u8g2.clear();
+    }
     
     else if (digitalRead(PIN_BAS) == HIGH)
     {
+        
         numOrdre = (numOrdre - 1) % NOMBRE_ORDRES;
-        u8g2.drawStr(10, 15, ordreTexte[numOrdre]);
+        u8g2.firstPage();
+        do 
+        {
+          u8g2.setFont(u8g2_font_ncenB14_tr);
+          u8g2.drawStr(10, 50, ordreTexte[numOrdre]);
+        } while (u8g2.nextPage());
+        u8g2.clear();
     }
     
     else if (digitalRead(PIN_PLAY) == HIGH)
     {
-        u8g2.drawStr(10, 15, "En cours : ");
-        u8g2.drawStr(10, 30, ordreTexte[numOrdre]);
+        
+        u8g2.firstPage();
+        do 
+        {
+          u8g2.setFont(u8g2_font_ncenB14_tr);
+          u8g2.drawStr(10, 20, "En cours : ");
+          u8g2.drawStr(10, 50, ordreTexte[numOrdre]);
+        } while (u8g2.nextPage());
         send_data.mode = ordre[numOrdre];
+        u8g2.clear();
     }
     
     else if (digitalRead(PIN_PAUSE) == HIGH)
     {
-        u8g2.drawStr(10, 15, "Pause !");
+        
+        do 
+        {
+          u8g2.setFont(u8g2_font_ncenB14_tr);
+          u8g2.drawStr(10, 50, "Pause");
+        } while (u8g2.nextPage());
         send_data.mode = PAUSE;
+        u8g2.clear();
     }
     
     else if (digitalRead(PIN_STOP) == HIGH)
     {
-        u8g2.drawStr(10, 15, "STOP !");
+        
+        do 
+        {
+          u8g2.setFont(u8g2_font_ncenB14_tr);
+          u8g2.drawStr(10, 50, "STOP");
+        } while (u8g2.nextPage());
         send_data.mode = STOP;
+        u8g2.clear();
     }
     
     else
     {
-        u8g2.drawStr(10, 15, "En attente !");
+        
+        do 
+        {
+          u8g2.setFont(u8g2_font_ncenB14_tr);
+          u8g2.drawStr(10, 50, "En attente");
+        } while (u8g2.nextPage());
         send_data.mode = RIEN;
+        //u8g2.clear();
     }
-  
-    //affichage à l'écran
-    u8g2.sendBuffer(); 
-  
+
     //envoie sur le réseau
     RF24NetworkHeader nHeader(noeudMere);
     network.write(nHeader, &send_data, sizeof(send_data));
     delay(100);
-  
+}
+
+void nettoieEcran()
+{
+    do 
+    {
+      for (int i = 0; i < 128; i++)
+      {
+        for (int j = 0; j < 100; j++)
+        {
+          u8g2.drawStr(i, j, " ");
+        }
+      }
+    } while (u8g2.nextPage());
 }
